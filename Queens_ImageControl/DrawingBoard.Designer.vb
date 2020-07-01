4d4f25ac74c8b5ce43000ee54969c615a058c0d5
Filet1323 <67002221+Filet1323@users.noreply.github.com>
2020-07-01 13:05:39 +0200

    Add files via upload

---
+++ Queens_ImageControl/DrawingBoard.Designer.vb
@@ -0,0 +1,33 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DrawingBoard
+    Inherits System.Windows.Forms.UserControl
+
+    '''UserControl1 zastepuje dispose, aby wyczyscic liste komponentow.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    '''Wymagane przez projektanta formularzy Windows
+    Private components As System.ComponentModel.IContainer
+
+    '''UWAGA: Projektant formularzy Windows wymaga nastepującej procedury
+    '''Mozna go zmodyfikowac za pomoca Projektanta formularzy Windows.
+    '''Nie modyfikuj go za pomoca edytora kodu.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '''
+        '''DrawingBoard
+        '''
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Name = "DrawingBoard"
+        Me.Size = New System.Drawing.Size(189, 165)
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
---
+++ Test Harness/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+'''Informacje ogólne o zespole są kontrolowane w następujący sposób
+'''zestaw atrybutów. Zmień te wartości atrybutów, aby zmodyfikować informacje
+'''związany z zespołem.
+
+'''Przejrzyj wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("Test Harness")> 
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Queen")>
+<Assembly: AssemblyProduct("Test Harness")>
+<Assembly: AssemblyCopyright("Przegladarka Queen 2020")>
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'''Poniższy identyfikator GUID dotyczy identyfikatora typelib, jeśli ten projekt jest wystawiony na COM
+<Assembly: Guid("31578221-4de2-465c-b87b-fa01fc0d56f1")>
+'''Informacje o wersji dla zestawu składają się z czterech następujących wartości:
+'''
+'''Wersja główna
+'''Wersja mniejsza
+'''      Numer kompilacji
+'''Rewizja
+'''
+'''Możesz określić wszystkie wartości lub domyślnie numery kompilacji i wersji
+'''za pomocą„ * ”, jak pokazano poniżej:
+'''<Assembly: AssemblyVersion („ 1.0. * ”)>
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
